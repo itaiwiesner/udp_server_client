ab57113984eff06865afb247b7ee52fe878cfec2
Itai Wiesner <itaiwies7@gmail.com>
2022-03-20 17:07:43 +0200

    Update server.py

--- server.py
+++ server.py
@@ -8,10 +8,13 @@ def filter_port(pkt):
     return UDP in pkt and 20000 <= pkt[UDP].dport <= 20256
 
 
-length = sniff(count=1, lfilter=filter_port)[0].dport - 2000
+length = sniff(count=1, lfilter=filter_port)[0].dport - 20000
+print(length)
 msg = []
-for _ in length:
-    msg.append(chr(sniff(count=1, lfilter=filter_port)[0].dport - 2000))
+for _ in range(length):
+    print(msg)
+    msg.append(chr(sniff(count=1, lfilter=filter_port)[0].dport - 20000))
 
+print('out')
 for i in msg:
     print(i, end='')
