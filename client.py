08b6347feca24ec9b1d85c045049aa088a9f765e
itaiwiesner <101206174+itaiwiesner@users.noreply.github.com>
2022-04-07 09:23:36 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ from scapy import *
 from scapy.layers.inet import IP, UDP
 from scapy.sendrecv import send
 
-PORT = 6969
-DEST_IP = '192.168.1.107'
+PORT = 20000
+DEST_IP # this is the ip of the server
 msg = input("enter your message: ")
 # create and send a packet representing the length of the message
 length_pkt = IP(dst=DEST_IP,) / UDP(sport=PORT, dport=len(msg) + 20000)
